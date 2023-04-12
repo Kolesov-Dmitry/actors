d139ea13dff1ae9a6078210c2c8c233bb060ff7c
Колесов Дмитрий Владимирович <dvkolesov@mts.ru>
2023-04-12 22:27:32 +0700

    fix response SetValue

--- actor/response.go
+++ actor/response.go
@@ -10,17 +10,21 @@ type Response struct {
 
 func newResponse() *Response {
 	return &Response{
-		value: make(chan any),
+		value: make(chan any, 1),
 	}
 }
 
 func (r *Response) SetValue(value any) {
-	r.value <- value
+	select {
+	case r.value <- value:
+	default:
+	}
 }
 
 func (r *Response) Result(ctx context.Context) (any, error) {
 	select {
 	case result := <-r.value:
+		close(r.value)
 		return result, nil
 
 	case <-ctx.Done():
